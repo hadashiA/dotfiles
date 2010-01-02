9a0de18cffa848d229b19aa258ce74393e1e96f4
hadshiA <dev@hadashikick.jp>
2010-01-03 04:26:31 +0900

    config.fish 整理。にサブディレクトリを追加指定

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,58 +8,16 @@ for i in $path_list
 	end
 end
 
-# $EDITOR
 set EDITOR /usr/bin/vim
-
-# $BROWSER
 set BROWSER "open -a Firefox"
-
-# $DEVDIR
-set DEVDIR "~/dev/"
-
-# autotest
 set RSPEC true
-
-# 
-# set ARCHFLAGS '-arch x86'
+#set ARCHFLAGS '-arch x86_64'
 
 # PostgreSQL
 set POSTGRES_INCLUDE /opt/local/include/postgresql84/
 set POSTGRES_LIB /opt/local/lib/postgresql84/
 
-# alias
-function lhome -d "cd ~/lib/home (git managiment home under files)"
-  cd ~/lib/home
-end
-
-function ssdudb
-  ssh ddunit_user@75.101.157.15
-end
-
-function ssdumemcached
-  ssh ddunit_user@75.101.244.44
-end
-
-function ssdu1 
-  ssh ddunit_user@174.129.140.240
-end
-
-function ssdu2 
-  ssh ddunit_user@174.129.185.69
-end
-
-function ssdu3 
-  ssh ddunit_user@174.129.182.142
-end
-
-function ssdu4 
-  ssh ddunit_user@75.101.240.26
-end
-
-function ssdu5 
-  ssh ddunit_user@174.129.79.192
-end
-
-function ssdustaging
-  ssh ddunit_user@staging.danceunit.com   
-end
+# set function sub directory
+set fish_function_path $fish_function_path ~/.config/fish/functions/override
+set fish_function_path $fish_function_path ~/.config/fish/functions/git
+set fish_function_path $fish_function_path ~/.config/fish/functions/rails
