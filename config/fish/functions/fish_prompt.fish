c300545e4218cf1f11fe45a8abd9328dd7a73f54
kubota <kubota@mario.local>
2010-01-06 20:19:47 +0900

    fish_prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt -d 'Write out the prompt'
-    printf '%s@%s%s%s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (git_cwd_info)
+    printf '%s@%s%s%s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd)  (set_color normal) (git_cwd_info)
 end 
 
