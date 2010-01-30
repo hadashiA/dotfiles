af020cf60266dc6b97311f0759acee754adcf16f
hadashi <12@hadashikick.jp>
2010-01-30 19:42:50 +0900

    vimperator/plugin

--- vimperator/plugin/lookupDictionary.js
+++ vimperator/plugin/lookupDictionary.js
@@ -7,34 +7,41 @@
  * @version 0.3
  * ==/VimperatorPlugin==
  */
-(function(){
+(function () {
 
 const SITE_DEFINITION = [{
     names: ['eiji[ro]'],
     url: 'http://eow.alc.co.jp/%s/UTF-8/',
-    shortHelp: 'SPACE ALC (英辞郎 on the Web)',
+    shortHelp: 'SPACE ALC (\u82F1\u8F9E\u6717 on the Web)',
     xpath: 'id("resultList")',
-    dictionary: 'en-US',
+    dictionary: 'en-US'
 },{
-    names: ['goo'],
-    url: 'http://dictionary.goo.ne.jp/search.php?MT=%s&kind=all&mode=0',
-    shortHelp: 'goo 辞書',
-    encode: 'EUC-JP',
-    xpath: '//div[@id="incontents"]/*[@class="ch04" or @class="fs14" or contains(@class,"diclst")]',
+    names: ['goo[dictionary]'],
+    url: 'http://dictionary.goo.ne.jp/search.php?MT=%s&kind=all&mode=0&IE=UTF-8',
+    shortHelp: 'goo \u8F9E\u66F8',
+    xpath: 'id("incontents")/*[@class="ch04" or @class="fs14" or contains(@class,"diclst")]',
     multi: true,
     dictionary: 'en-US',
-},{
-    names: ['infokanji'],
-    url: 'http://dictionary.www.infoseek.co.jp/?sc=1&se=on&lp=0&gr=kj&sv=KJ&qt=&qty=%s&qtb=&qtk=0',
-    shorthelp: 'Infoseek 漢字辞書',
-    encode: 'EUC-JP',
-    xpath: '//div[@class="NetDicHead"]',
+    srcEncode: 'EUC-jp',
+    urlEncode: 'UTF-8'
 },{
     names: ['answers'],
     url: 'http://www.answers.com/%s',
-    shortHelp: 'Answers.com(英英辞書)',
+    shortHelp: 'Answers.com(\u82F1\u82F1\u8F9E\u66F8)',
     xpath: 'id("firstDs")',
-    dictionary: 'en-US',
+    dictionary: 'en-US'
+},{
+    names: ['wikipe[diaja]'],
+    url: 'http://ja.wikipedia.org/wiki/%s',
+    shortHelp: 'Wikipedia lite(ja)',
+    xpath: 'id("bodyContent")/p[1]',
+    dictionary: 'ja'
+},{
+    names: ['wikipe[diaen]'],
+    url: 'http://en.wikipedia.org/wiki/%s',
+    shortHelp: 'Wikipedia lite(en)',
+    xpath: 'id("bodyContent")/p[1]',
+    dictionary: 'en'
 }];
 
 let (siteDef = liberator.globalVariables.lookupDictionary_site_definition) {
@@ -91,8 +98,8 @@ SpellChecker.prototype = {
      * @param {String} dict
      */
     setDictionary: function (dict) {
-        var dictionaries = this.getDictionaryList()
-        for (var i=0, max=dictionaries.length ; i<max ; ++i) {
+        var dictionaries = this.getDictionaryList();
+        for (let i=0, max=dictionaries.length ; i<max ; ++i) {
             if (dictionaries[i] === dict) {
                 this.engine.dictionary = dict;
                 return dict;
@@ -137,34 +144,36 @@ SpellChecker.prototype = {
 
 var spellChecker = buildSpellChecker();
 
-SITE_DEFINITION.forEach(function(dictionary){
+SITE_DEFINITION.forEach(function (dictionary) {
     commands.addUserCommand(
         dictionary.names,
         dictionary.shortHelp,
-        function(arg,special){
+        function (args) {
+            var arg = args.string;
+
             var sel = (window.content.window.getSelection) ?
                 window.content.window.getSelection().toString() : null;
-            if (special && sel) arg = sel;
+            if (args.bang && sel) arg = sel;
             if (!arg) return;
             var url;
-            if (dictionary.encode){
-                var ttbu = Components.classes['@mozilla.org/intl/texttosuburi;1']
+            if (dictionary.urlEncode) {
+                let ttbu = Components.classes['@mozilla.org/intl/texttosuburi;1']
                                      .getService( Components.interfaces.nsITextToSubURI);
-                url = dictionary.url.replace(/%s/g, ttbu.ConvertAndEscape(dictionary.encode, arg));
+                url = dictionary.url.replace(/%s/g, ttbu.ConvertAndEscape(dictionary.urlEncode, arg));
             } else {
-                url = dictionary.url.replace(/%s/g,encodeURI(arg));
+                url = dictionary.url.replace(/%s/g,encodeURIComponent(arg));
             }
             //liberator.log('URL: ' +url);
             var result;
-            getHTML(url, function(str){
+            getHTML(url, function (str) {
                 var doc = createHTMLDocument(str);
                 var result = getNodeFromXPath(dictionary.xpath, doc, dictionary.multi);
-                if (!result){
+                if (!result) {
                     liberator.echoerr('Nothing to show...');
                 }
                 var xs = new XMLSerializer();
-                liberator.echo('<base href="' + url + '"/>' + xs.serializeToString( result ), true);
-            }, dictionary.encode ? dictionary.encode : 'UTF-8');
+                liberator.echo(new XMLList('<div style="white-space:normal;"><base href="' + util.escapeHTML(url) + '"/>' + xs.serializeToString( result ).replace(/<[^>]+>/g,function (all) all.toLowerCase() ) + '</div>'), true);
+            }, dictionary.srcEncode ? dictionary.srcEncode : null);
         },
         {
             completer: function (arg) {
@@ -175,7 +184,7 @@ SITE_DEFINITION.forEach(function(dictionary){
 
                 var suggestions = spellChecker.suggest(arg);
                 var candidates = [];
-                for (var i=0, max=suggestions.length ; i<max ; ++i) {
+                for (let i=0, max=suggestions.length ; i<max ; ++i) {
                     candidates.push([suggestions[i], 'suggest']);
                 }
 
@@ -199,11 +208,11 @@ commands.addUserCommand(
  * @param {Function} callback
  * @param {String} charset
  */
-function getHTML(url, callback, charset){
+function getHTML(url, callback, charset) {
     var xhr= new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        if (xhr.readyState == 4){
-            if (xhr.status == 200){
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
                 callback.call(this,xhr.responseText);
             } else {
                 throw new Error(xhr.statusText);
@@ -211,17 +220,17 @@ function getHTML(url, callback, charset){
         }
     };
     xhr.open('GET',url,true);
-    xhr.overrideMimeType('text/html; charset=' + charset);
+    if (charset) xhr.overrideMimeType('text/html; charset=' + charset);
     xhr.send(null);
 }
 /**
  * @param {String} str
  * @return {DOMDocument}
  */
-function createHTMLDocument(str){
-    str = str.replace(/^[\s\S]*?<html(?:\s[^>]+?)?>|<\/html\s*>[\S\s]*$/ig,'').replace(/[\r\n]+/g,' ');
-    var htmlFragment = document.implementation.createDocument(null,'html',null);
-    var range = document.createRange();
+function createHTMLDocument(str) {
+    str = str.replace(/^[\s\S]*?<html(?:[ \t\r\n][^>]*)?>[ \t\n\r]*|[ \t\n\r]*<\/html[ \t\r\n]*>[\S\s]*$/ig,'').replace(/[\r\n]+/g,' ');
+    var htmlFragment = content.document.implementation.createDocument(null,'html',null);
+    var range = content.document.createRange();
     range.setStartAfter(window.content.document.body);
     htmlFragment.documentElement.appendChild(htmlFragment.importNode(range.createContextualFragment(str),true));
     return htmlFragment;
@@ -232,16 +241,16 @@ function createHTMLDocument(str){
  * @param {Boolean} isMulti
  * @return {Element}
  */
-function getNodeFromXPath(xpath,doc,isMulti){
+function getNodeFromXPath(xpath,doc,isMulti) {
     if (!xpath || !doc) return;
     var result;
-    if (isMulti){
-        var nodesSnapshot = doc.evaluate(xpath,doc.documentElement,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+    if (isMulti) {
+        let nodesSnapshot = doc.evaluate(xpath,doc.documentElement,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
         if (nodesSnapshot.snapshotLength == 0) return;
         result = document.createElementNS(null,'div');
-        for (var i=0; i<nodesSnapshot.snapshotLength; result.appendChild(nodesSnapshot.snapshotItem(i++)));
+        for (let i=0; i<nodesSnapshot.snapshotLength; result.appendChild(nodesSnapshot.snapshotItem(i++)));
     } else {
-        var node = doc.evaluate(xpath,doc.documentElement,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
+        let node = doc.evaluate(xpath,doc.documentElement,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
         if (!node.singleNodeValue) return;
         result = node.singleNodeValue;
     }
@@ -258,7 +267,7 @@ function buildSpellChecker() {
 
     var spellChecker = new SpellChecker();
 
-    var isBeginningWith = liberator.globalVariables.lookupDictionary_beginningWith
+    var isBeginningWith = liberator.globalVariables.lookupDictionary_beginningWith;
     isBeginningWith = (isBeginningWith === undefined) ? false : !!parseInt(isBeginningWith, 10);
     spellChecker.setBeginningWith(isBeginningWith);
 
