c81d60e0e2eee1bd153a0e1ccae69e990607db4f
hadshiA <dev@hadashikick.jp>
2010-02-03 02:26:30 +0900

    emacs

--- emacs.d/conf/init-global.el
+++ emacs.d/conf/init-global.el
@@ -121,9 +121,9 @@
 
 ;; バッファ全部を自動的に保存・復元
 ;; http://www.hasta-pronto.org/archives/2008/01/30-0235.php
-(autoload 'save-current-configuration "revive" "Save status" t)
-(autoload 'resume "revive" "Resume Emacs" t)
-(autoload 'wipe "revive" "Wipe emacs" t)
-(define-key ctl-x-map "R" 'resume)                        ; C-x R で復元
-(define-key ctl-x-map "K" 'wipe)                          ; C-x K で Kill
-(add-hook 'kill-emacs-hook 'save-current-configuration)   ; 終了時に保存
+;;(autoload 'save-current-configuration "revive" "Save status" t)
+;;(autoload 'resume "revive" "Resume Emacs" t)
+;;(autoload 'wipe "revive" "Wipe emacs" t)
+;;(define-key ctl-x-map "R" 'resume)                        ; C-x R で復元
+;;(define-key ctl-x-map "K" 'wipe)                          ; C-x K で Kill
+;;(add-hook 'kill-emacs-hook 'save-current-configuration)   ; 終了時に保存
