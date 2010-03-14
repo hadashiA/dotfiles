aa0d65a4d4bc442cdd3e158a8fed1a2084603676
hadshiA <dev@hadashikick.jp>
2010-03-14 22:35:34 +0900

    使ってないeshellとかコメントアウト

--- emacs.d/conf/init-shell.el
+++ emacs.d/conf/init-shell.el
@@ -5,16 +5,16 @@
 
 ;; term-mode
 ;; http://dev.ariel-networks.com/Members/matsuyama/pseudo-screen-on-emacs-with-elscreen-and-term
-(require 'term)
+;; (require 'term)
 
-;; term-modeで、C-xがC-cに割り当てられるのを防ぐ
-(term-set-escape-char ?\C-x)
+;; ;; term-modeで、C-xがC-cに割り当てられるのを防ぐ
+;; (term-set-escape-char ?\C-x)
 
-;; term-modeが、C-z(elscreenのプレフィックスキー)をパクるのを防ぐ
-(add-hook 'term-mode-hook
-          '(lambda ()
-             (define-key term-raw-map "\C-z"
-               (lookup-key (current-global-map) "\C-z"))))
+;; ;; term-modeが、C-z(elscreenのプレフィックスキー)をパクるのを防ぐ
+;; (add-hook 'term-mode-hook
+;;           '(lambda ()
+;;              (define-key term-raw-map "\C-z"
+;;                (lookup-key (current-global-map) "\C-z"))))
 
 ;; shell-mode
 ;; パスワードを隠す
@@ -30,19 +30,19 @@
 
 ;; eshell
 ;; http://www.bookshelf.jp/pukiwiki/pukiwiki.php?Eshell%A4%F2%BB%C8%A4%A4%A4%B3%A4%CA%A4%B9
-(require 'eshell)
+;; (require 'eshell)
 
-;; C-aをbegnning-of-lineではなく、コマンド文字列の先頭に行くようにする
-(add-hook 'eshell-mode-hook
-          '(lambda ()
-             (define-key eshell-mode-map (kbd "C-a") 'eshell-bol)))
+;; ;; C-aをbegnning-of-lineではなく、コマンド文字列の先頭に行くようにする
+;; (add-hook 'eshell-mode-hook
+;;           '(lambda ()
+;;              (define-key eshell-mode-map (kbd "C-a") 'eshell-bol)))
 
-;; .*をzshと同じくようにマッチさせる
-;; デフォルトでは、.*で親ディレクトリにマッチする
-;; http://d.hatena.ne.jp/Rommy/20070115/1168876829
-(setq eshell-glob-include-dot-dot nil)
+;; ;; .*をzshと同じくようにマッチさせる
+;; ;; デフォルトでは、.*で親ディレクトリにマッチする
+;; ;; http://d.hatena.ne.jp/Rommy/20070115/1168876829
+;; (setq eshell-glob-include-dot-dot nil)
 
-;; Meadow Memo Wikiより
-;; http://www.bookshelf.jp/pukiwiki/pukiwiki.php?Eshell%A4%F2%BB%C8%A4%A4%A4%B3%A4%CA%A4%B9#content_1_31
-(setq eshell-ask-to-save-history (quote always))
-(setq eshell-history-size 100000)
+;; ;; Meadow Memo Wikiより
+;; ;; http://www.bookshelf.jp/pukiwiki/pukiwiki.php?Eshell%A4%F2%BB%C8%A4%A4%A4%B3%A4%CA%A4%B9#content_1_31
+;; (setq eshell-ask-to-save-history (quote always))
+;; (setq eshell-history-size 100000)
