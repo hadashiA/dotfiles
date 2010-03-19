97ae16228191f81c64e63b8f0b73d34c25c61bf5
hadshiA <dev@hadashikick.jp>
2010-03-20 03:17:47 +0900

    fish /usr/local/git/bin:/opt/local/lib/postgresql84/bin:/opt/local/lib/mysql5/bin:/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin/:/usr/local/git/bin/:~/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/opt/local/lib/postgresql84/bin:/opt/local/sbin:/opt/local/bin:/Users/hadashi/bin:/usr/X11R6/bin:/usr/local/jruby/bin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,28 @@
 # $PATH
-set -l path_list $HOME/bin /opt/local/bin /opt/local/sbin /opt/local/lib/mysql5/bin/ /opt/local/lib/postgresql84/bin/ /usr/local/git/bin /usr/local/mysql/bin
-for i in $path_list
-	if not contains $i $PATH
-		if test -d $i
-			set PATH $PATH $i
-		end
-	end
-end
+# set -l path_list $HOME/bin /opt/local/bin /opt/local/sbin /opt/local/lib/mysql5/bin/ /opt/local/lib/postgresql84/bin/ /usr/local/git/bin /usr/local/mysql/bin
+# for i in $path_list
+#     if not contains $i $PATH
+#         if test -d $i
+#            echo $PATH
+#            set PATH $PATH $i
+# 	end
+#     end
+# end
+
+set PATH ~/bin $PATH
+set PATH /usr/local/git/bin/ $PATH
+set PATH /usr/local/mysql/bin/ $PATH
+set PATH /opt/local/bin $PATH
+set PATH /opt/local/sbin $PATH
+set PATH /opt/local/lib/postgresql84/bin $PATH
+
+set ARCHFLAGS='-arch x86'
+set RSPEC=true
+
+# PostgreSQL
+# set POSTGRES_INCLUDE=/opt/local/include/postgresql84/
+# set POSTGRES_LIB=/opt/local/lib/postgresql84/
+
 
 set EDITOR /usr/bin/vim
 set BROWSER "open -a Firefox"
