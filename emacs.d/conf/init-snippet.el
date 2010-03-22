f0672d54e9213bacd6aad2baa34bebc5e45b62ed
hadshiA <dev@hadashikick.jp>
2010-03-22 15:10:19 +0900

    keybindingをデフォルトに。ruby-modeだけなぜか動かない

--- emacs.d/conf/init-snippet.el
+++ emacs.d/conf/init-snippet.el
@@ -3,10 +3,9 @@
  
 ;;(when nil ;(require 'yasnippet nil t) ; pending.
 (when (require 'yasnippet nil)
-  (setq yas/root-directory (list (expand-file-name "~/.emacs.d/snippets/"))
-        yas/use-menu nil
-        yas/trigger-key (kbd "SPC") ; default => "TAB"
-        yas/next-field-key (kbd "TAB") ; default => "TAB"
+  (setq yas/use-menu nil
+        ;; yas/trigger-key (kbd "SPC")    ; default => "TAB"
+        ;; yas/next-field-key (kbd "TAB")
         yas/buffer-local-condition
         '(or (not (or (string= "font-lock-comment-face"
                                (get-char-property (point) 'face))
@@ -17,27 +16,6 @@
   (and (require 'dropdown-list nil t)
        (setq yas/text-popup-function
              #'yas/dropdown-list-popup-for-template))
- 
-  (defvar snippet-directories nil)
- 
-  (mapc '(lambda (path)
-           (and
-            (file-accessible-directory-p path)
-            (add-to-list 'snippet-directories
-                         (expand-file-name path))))
-        (list ;;"~/src/lang/elisps/yasnippet-snippets/common" ; CodeRepos
-              ;;"~/src/lang/elisps/yasnippet-snippets/elim" ; CodeRepos
-              "~/.emacs.d/snippets")) ; Private
- 
-  ;; yasnippet公式提供のものと、自分用カスタマイズスニペットをロード同名
-  ;; のスニペットが複数ある場合、あとから読みこんだ自分用のものが優先され
-  ;; る。また、スニペットを変更、追加した場合、このコマンドを実行すること
-  ;; で、変更・追加が反映される。
- 
-  (defun yas/load-all-directories ()
-    (interactive)
-    (yas/reload-all)
-    (mapc 'yas/load-directory-1 snippet-directories))
  
   ;;; [2008-03-17]
   ;;; yasnippet展開中はflymakeを無効にする
@@ -67,5 +45,4 @@
  
   ;; yasnippet初期化
   (yas/initialize)
-  (yas/load-all-directories))
- 
+  (yas/load-directory (expand-file-name "~/.emacs.d/snippets")))
