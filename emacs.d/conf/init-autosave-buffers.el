06bd4358f6ac27019f2e0c511b25c14d655d5a79
hadshiA <dev@hadashikick.jp>
2010-04-04 14:39:25 +0900

    .omm non autosave

--- emacs.d/conf/init-autosave-buffers.el
+++ emacs.d/conf/init-autosave-buffers.el
@@ -6,5 +6,5 @@
 (when (require 'auto-save-buffers-enhanced)
   (auto-save-buffers-enhanced t)
   ;; (setq auto-save-buffers-enhanced-include-regexps '(".+"))
-  ;; (setq auto-save-buffers-enhanced-exclude-regexps '("^not-save-file" "\\.ignore$"))
+  (setq auto-save-buffers-enhanced-exclude-regexps '("^not-save-file" "\\.ignore$" ".*\\.omm$"))
   (global-set-key "\C-xas" 'auto-save-buffers-enhanced-toggle-activity))
