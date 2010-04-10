5895b9f9d4666a0813f7a70e9ac1fe695a4a442c
hadshiA <dev@hadashikick.jp>
2010-04-10 23:48:12 +0900

    vimlike command update

--- emacs.d/conf/init-vimlike.el
+++ emacs.d/conf/init-vimlike.el
@@ -1,11 +1,32 @@
-(defun search-to-char-forward (arg char)
-  (interactive "p\ncSearch to forward char: ")
-  (search-forward (char-to-string char) nil nil arg)
-  (backward-char))
+(defvar vimlike-f-recent-char nil)
+(defvar vimlike-f-recent-func nil)
 
-(defun search-to-char-backward (arg char)
-  (interactive "p\ncSearch to backward char: ")
-  (search-backward (char-to-string char) nil nil arg))
+(defun vimlike-f (char)
+  "search to forward char into current line and move point (vim 'f' command)"
+  (interactive "cSearch to forward char: ")
+  (when (= (char-after (point)) char)
+    (forward-char))
+  (search-forward (char-to-string char) (point-at-eol) nil 1)
+  (backward-char)
+  (setq vimlike-f-recent-search-char char
+        vimlike-f-recent-search-func 'vimlike-f))
 
-(global-set-key "\M-s" 'search-to-char-forward)
-(global-set-key "\M-r" 'search-to-char-backward)
+(defun vimlike-F (char)
+  "search to forward char into current line and move point. (vim 'F' command)"
+  (interactive "cSearch to backward char: ")
+  (search-backward (char-to-string char) (point-at-bol) nil 1)
+  (setq vimlike-f-recent-search-char char
+        vimlike-f-recent-search-func 'vimlike-F))
+
+(defun vimlike-semicolon ()
+  "search repeat recent vimlike 'f' or 'F' search char (vim ';' command)"
+  (interactive)
+  (if (and vimlike-f-recent-search-char
+             vimlike-f-recent-search-func)
+      (funcall vimlike-f-recent-search-func vimlike-f-recent-search-char)
+    (message "Empty recent search char.")))
+
+(global-set-key "\M-s" 'vimlike-f)
+(global-set-key "\M-r" 'vimlike-F)
+(global-set-key "\M-j" 'vimlike-semicolon)
+
