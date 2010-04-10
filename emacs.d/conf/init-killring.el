9a3644f4e77651277da8744c78f37effb732b21d
hadshiA <dev@hadashikick.jp>
2010-04-10 20:30:34 +0900

    kill ring M-Y

--- emacs.d/conf/init-killring.el
+++ emacs.d/conf/init-killring.el
@@ -1,13 +1,17 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;; kill-summary C-yした後にM-yすると、候補を表示するウィンドウが現れる
-;; http://mibai.tec.u-ryukyu.ac.jp/~oshiro/Programs/elisp/kill-summary.el
-;;
-;; [使い方]
-;;   p/n(or j/k)で上下移動しながらkill-ringから文字列を置き換える
-;;   C-p/C-nだと、候補を移動するだけ
-;;   SPC で現在行を選択．C-v (scroll-up) でスクロール
-;;   d で現在行のキルリングを即座に消去
+;; M-Y で行のコピー
+(defun duplicate-line (&optional numlines)
+  "One line is duplicated wherever there is a cursor."
+  (interactive "p")
+  (let* ((col (current-column))
+         (bol (progn (beginning-of-line) (point)))
+         (eol (progn (end-of-line) (point)))
+         (line (buffer-substring bol eol)))
+    (while (> numlines 0)
+      (insert "\n" line)
+      (setq numlines (- numlines 1)))
+    (move-to-column col)))
 
-(autoload 'kill-summary "kill-summary" nil t)
-(global-set-key "\M-y" 'kill-summary)
+(define-key esc-map "Y" 'duplicate-line)
+
