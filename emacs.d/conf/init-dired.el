cdea6924ba23a1d635db2144b3181e56dc868bf4
kubota <kubota@mario.local>
2010-04-21 20:31:13 +0900

    init-dired

--- emacs.d/conf/init-dired.el
+++ emacs.d/conf/init-dired.el
@@ -2,19 +2,29 @@
 
 ;; wdired
 ;; http://www.bookshelf.jp/soft/meadow_25.html#SEC292
-(require 'wdired)
+(when (require 'wdired nil t)
+  ;; rでdiredバッファを直接編集できるようになる。
+  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
-;; rでdiredバッファを直接編集できるようになる。
-(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+  ;;パーミッションも変更可能に
+  (setq wdired-allow-to-change-permissions t))
 
-;;パーミッションも変更可能に
-(setq wdired-allow-to-change-permissions t)
 
 ;; lsの代りにEmacs自体の機能を利用してディレクトリをリスト
 ;; http://www.namazu.org/~tsuchiya/elisp/#dired
-(require 'ls-lisp)
-(let (current-load-list)
-  (defadvice insert-directory
-    (around reset-locale activate compile)
-    (let ((system-time-locale "C"))
-      ad-do-it)))
+(when (require 'ls-lisp nil t)
+  (let (current-load-list)
+    (defadvice insert-directory
+      (around reset-locale activate compile)
+      (let ((system-time-locale "C"))
+        ad-do-it))))
+
+;; (when (require 'dired-x)
+;;   (setq dired-guess-shell-alist-user
+;;         '(("\\.tar\\.gz\\'"  "tar ztvf")
+;;           ("\\.taz\\'" "tar ztvf")
+;;           ("\\.tar\\.bz2\\'" "tar Itvf")
+;;           ("\\.zip\\'" "unzip -l")
+;;           ("\\.\\(g\\|\\) z\\'" "zcat")
+;;           ("\\.\\(jpg\\|JPG\\|gif\\|GIF\\)\\'" (when run-carbon-emacs "open"))
+;;           )))
