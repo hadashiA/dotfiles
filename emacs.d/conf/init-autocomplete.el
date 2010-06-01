6e63732e22838afbe6762d5373f7e5b8e5861a9e
kubota <kubota@mario.local>
2010-06-01 17:56:04 +0900

    auto-complete.el update v1.3

--- emacs.d/conf/init-autocomplete.el
+++ emacs.d/conf/init-autocomplete.el
@@ -1,32 +1,34 @@
 ;; Auto Complete Mode
 ;; http://cx4a.org/software/auto-complete/manual.ja.html#ac-source-yasnippet
-(when (require 'auto-complete nil t)
+;; (when (require 'auto-complete nil t)
+(when (require 'auto-complete-config nil t)
   ;; (ac-config-default)
   (global-auto-complete-mode t)
   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 
-  (setq ac-sources
-        '(;; ac-source-abbrev
-          ;; ac-source-dictionary
-          ;; ac-source-eclim
-          ;; ac-source-features            ; Emacs Lisp
-          ac-source-filename
-          ;; ac-source-files-in-current-dir
-          ;; ac-source-functions           ; Emacs Lisp
-          ;; ac-source-gtags
-          ;; ac-source-imenu
-          ;; ac-source-semantic
-          ;; ac-source-symbols             ; Emacs Lisp
-          ;; ac-source-variables           ; Emacs Lisp
-          ;; ac-source-words-in-all-buffer
-          ;; ac-source-words-buffer
-          ac-source-words-in-same-mode-buffers
-          ac-source-yasnippet))
+  ;; (setq ac-sources
+  ;;       '(;; ac-source-abbrev
+  ;;         ;; ac-source-dictionary
+  ;;         ;; ac-source-eclim
+  ;;         ;; ac-source-features            ; Emacs Lisp
+  ;;         ac-source-filename
+  ;;         ;; ac-source-files-in-current-dir
+  ;;         ;; ac-source-functions           ; Emacs Lisp
+  ;;         ;; ac-source-gtags
+  ;;         ;; ac-source-imenu
+  ;;         ;; ac-source-semantic
+  ;;         ;; ac-source-symbols             ; Emacs Lisp
+  ;;         ;; ac-source-variables           ; Emacs Lisp
+  ;;         ;; ac-source-words-in-all-buffer
+  ;;         ;; ac-source-words-buffer
+  ;;         ac-source-words-in-same-mode-buffers
+  ;;         ac-source-yasnippet))
         
   ;; 色
-  (set-face-background 'ac-candidate-face "lightgray")
-  (set-face-underline 'ac-candidate-face "darkgray")
-  (set-face-background 'ac-selection-face "steelblue")
+  ;; (set-face-background 'ac-candidate-face "lightgray")
+  ;; (set-face-underline 'ac-candidate-face "darkgray")
+  ;; (set-face-background 'ac-selection-face "steelblue")
 
   ;; 補完対象に大文字が含まれる場合のみ区別する
   ;; (setq ac-ignore-case 'smart)
@@ -48,4 +50,6 @@
   (add-hook 'ruby-mode-hook
             (lambda ()
               (add-to-list 'ac-sources 'ac-source-rsense-method)
-              (add-to-list 'ac-sources 'ac-source-rsense-constant))))
+              (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+  (ac-config-default))
