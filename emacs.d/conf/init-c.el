d3b39c2eeac25ac6fc6424428cd0385e37ca24bb
hadshiA <dev@hadashikick.jp>
2010-08-09 01:16:32 +0900

    objc

--- emacs.d/conf/init-c.el
+++ emacs.d/conf/init-c.el
@@ -16,3 +16,40 @@
                (c-toggle-hungry-state t)
                (c-toggle-auto-state t)               
                (setq c-basic-offset 4 indent-tabs-mode nil)))
+
+(setq ff-other-file-alist
+      '(("\\.mm?$" (".h"))
+        ("\\.cc$"  (".hh" ".h"))
+        ("\\.hh$"  (".cc" ".C"))
+
+        ("\\.c$"   (".h"))
+        ("\\.h$"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
+
+        ("\\.C$"   (".H"  ".hh" ".h"))
+        ("\\.H$"   (".C"  ".CC"))
+
+        ("\\.CC$"  (".HH" ".H"  ".hh" ".h"))
+        ("\\.HH$"  (".CC"))
+
+        ("\\.cxx$" (".hh" ".h"))
+        ("\\.cpp$" (".hpp" ".hh" ".h"))
+        
+        ("\\.hpp$" (".cpp" ".c"))))
+
+(add-hook 'cc-mode-hook
+          (lambda ()
+            (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
+            ))
+
+(add-hook 'objc-mode-hook
+          (lambda ()
+            (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
+            ))
+
+
+
+
+
+
+
+
--- emacs.d/conf/init-objc.el
+++ emacs.d/conf/init-objc.el
@@ -12,29 +12,6 @@
 ;; ;; ffap-kpathsea-expand-path で展開するパスの深さ
 ;; (setq ffap-kpathsea-depth 5)
 
-(setq ff-other-file-alist
-      '(("\\.mm?$" (".h"))
-        ("\\.cc$"  (".hh" ".h"))
-        ("\\.hh$"  (".cc" ".C"))
-
-        ("\\.c$"   (".h"))
-        ("\\.h$"   (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m" ".mm"))
-
-        ("\\.C$"   (".H"  ".hh" ".h"))
-        ("\\.H$"   (".C"  ".CC"))
-
-        ("\\.CC$"  (".HH" ".H"  ".hh" ".h"))
-        ("\\.HH$"  (".CC"))
-
-        ("\\.cxx$" (".hh" ".h"))
-        ("\\.cpp$" (".hpp" ".hh" ".h"))
-        
-        ("\\.hpp$" (".cpp" ".c"))))
-
-(add-hook 'objc-mode-hook
-          (lambda ()
-            (define-key c-mode-base-map (kbd "C-c o") 'ff-find-other-file)
-            ))
 
 ;; ドキュメントの参照
 (when (require 'xcode-document-viewer) nil t
