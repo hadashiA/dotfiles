9352007b694351e32ff82b9d1a147071b068b3ce
kubota <kubota@mario.local>
2010-08-24 12:32:19 +0900

    delete-pair error-handling

--- emacs.d/conf/init-matelike.el
+++ emacs.d/conf/init-matelike.el
@@ -6,48 +6,56 @@
     (?\' . ?\'))
   "*pair of parentheses alist")
 
-;; (defadvice backward-delete-char-untabify
-;;   (before matelike-backward-delete-bracket activate)
-;;   (save-excursion
-;;     (let ((pair (assoc (char-after (1- (point))) my-delete-pair-alist))
-;;           (bracket-start (1- (point)))
-;;           (bracket-end nil))
-;;       (while (and (pair)
-;;                   (setq bracket-end
-;;                    (search-forward (char-to-string (cdr pair)) (point-max) t 1)))
-;;         (if (search-backward (char-to-string (car pair)) bracket-start)
-;;             (delete-char 1)
-;;         )))))
-
-;; (defun matelike-backward-delete-pair ()
-;;   "backward char and delete-pair"
+;; (defun matelike-delete-pair ()
 ;;   (interactive)
-;;   (if (assoc (char-before (point)) matelike-delete-pair-alist)
-;;       (progn
-;;         (backward-char)
-;;         (delete-pair))
-;;     (backward-delete-char-untabify 1)))
-;; (global-set-key "\C-h" 'matelike-backward-delete-pair)
+;;   (save-excursion
+;;     (let ((pair (assoc (char-after (point)) matelike-delete-pair-alist))
+;;           (pair-start-point (point))
+;;           (pair-end-point nil)
+;;           (count 1))
 
-(defun matelike-delete-pair ()
-  "wrap delete-pair only specify char"
-  (interactive)
-  (if (assoc (char-after (point)) matelike-delete-pair-alist)
-      (delete-pair)
-    (delete-char 1)))
+;;       (while (and pair
+;;                   (search-forward (char-to-string (cdr pair)) (point-max) t count))
+;;         (setq pair-end-point (point))
+;;         (if (and
+;;              (search-backward (char-to-string (car pair)) pair-start-point t count)
+;;              (= (point) pair-start-point))
+;;             (setq pair nil)
+;;           (progn
+;;             (setq count (1+ count))
+;;             (goto-char pair-start-point)
+;;             ))
+;;         )
 
-(global-set-key "\C-d" 'matelike-delete-pair)
+;;       (if (and pair pair-start-point pair-end-point)
+;;           (progn
+;;             (message (char-to-string (char-before  pair-start-point)))
+;;             (message (char-to-string (char-before  pair-end-point)))
+;;             ;; (goto-char pair-start-point)
+;;             ;; (delete-char 1)
+;;             ;; (goto-char pair-end-point)
+;;             ;; (delete-char 1)
+;;             ))
+;;       )))
 
 (defadvice backward-delete-char-untabify
   (around matelike-backward-delete-pair activate)
   (if (assoc (char-before (point)) matelike-delete-pair-alist)
       (progn
         (backward-char)
-        (delete-pair))
+        (condition-case nil
+            (delete-pair)
+          (error (progn
+                   (forward-char)
+                   ad-do-it))))
     ad-do-it))
 
-;; (defadvice delete-char
-;;   (around matelike-delete-pair activate)
-;;   (if (assoc (char-after (point)) matelike-delete-pair-alist)
-;;       (delete-char 1)
-;;     ad-do-it))
+(defun matelike-delete-pair ()
+  "wrap delete-pair only specify char"
+  (interactive)
+  (if (assoc (char-after (point)) matelike-delete-pair-alist)
+      (condition-case nil
+          (delete-pair)
+        (error (delete-char 1)))
+    (delete-char 1)))
+(global-set-key "\C-d" 'matelike-delete-pair)
