41b0f6d91ff76ae33f3fd81604d79bf32a4086fa
kubota <kubota@mario.local>
2010-10-18 18:55:12 +0900

    twitetring-mode

--- emacs.d/conf/init-twitter.el
+++ emacs.d/conf/init-twitter.el
@@ -4,11 +4,13 @@
 (require 'twittering-mode)
 (setq twittering-auth-method 'xauth)
 (setq twittering-username "hadahsiA")
+(setq twittering-status-format "%i %s %t %@")
 (setq twittering-timer-interval 75)
 (setq twittering-convert-fix-size 48)
 (setq twittering-update-status-function 'twittering-update-status-from-pop-up-buffer)
-(setq twittering-icon-mode t)
+(setq twittering-icon-mode nil)
 (setq twittering-scroll-mode nil)
+
 ;; いくつかのTLをまとめて名前をつけることができる
 (setq twittering-timeline-spec-alias
       `(("related-to" .
@@ -19,27 +21,27 @@
 	      ":home")))
 	))
 ;; 起動時に以下のリストを読みこむ
-(setq twittering-initial-timeline-spec-string
-      '("$related-to(atauky)"
-	"atauky/who-i-met"
-	"atauky/conversationlist"
-	"atauky/jef-united"
-	":direct_messages"
-	":home"))
-(add-hook 'twittering-mode-hook
-          (lambda ()
-            (set-face-bold-p 'twittering-username-face t)
-            (set-face-foreground 'twittering-username-face "DeepSkyBlue3")
-            (set-face-foreground 'twittering-uri-face "gray60")
-	    (setq twittering-status-format "%i %p%s / %S:\n%FOLD{%T}\n%r %R [%@]")
-	    (setq twittering-retweet-format " RT @%s: %t")
-            ;; "F"でお気に入り
-            ;; "R"でリツイートできるようにする
-            (define-key twittering-mode-map (kbd "F") 'twittering-favorite)
-            (define-key twittering-mode-map (kbd "R") 'twittering-native-retweet)
-            ;; "<"">"で先頭、最後尾にいけるように
-            (define-key twittering-mode-map (kbd "<") (lambda () (interactive) (goto-char (point-min))))
-            (define-key twittering-mode-map (kbd ">") (lambda () (interactive) (goto-char (point-max))))))
+;; (setq twittering-initial-timeline-spec-string
+;;       '("$related-to(atauky)"
+;; 	"atauky/who-i-met"
+;; 	"atauky/conversationlist"
+;; 	"atauky/jef-united"
+;; 	":direct_messages"
+;; 	":home"))
+;; (add-hook 'twittering-mode-hook
+;;           (lambda ()
+;;             (set-face-bold-p 'twittering-username-face t)
+;;             (set-face-foreground 'twittering-username-face "DeepSkyBlue3")
+;;             (set-face-foreground 'twittering-uri-face "gray60")
+;; 	    (setq twittering-status-format "%i %p%s / %S:\n%FOLD{%T}\n%r %R [%@]")
+;; 	    (setq twittering-retweet-format " RT @%s: %t")
+;;             ;; "F"でお気に入り
+;;             ;; "R"でリツイートできるようにする
+;;             (define-key twittering-mode-map (kbd "F") 'twittering-favorite)
+;;             (define-key twittering-mode-map (kbd "R") 'twittering-native-retweet)
+;;             ;; "<"">"で先頭、最後尾にいけるように
+;;             (define-key twittering-mode-map (kbd "<") (lambda () (interactive) (goto-char (point-min))))
+;;             (define-key twittering-mode-map (kbd ">") (lambda () (interactive) (goto-char (point-max))))))
 ;; URL短縮サービスをj.mpに
 ;; YOUR_USER_IDとYOUR_API_KEYを自分のものに置き換えてください
 ;; from http://u.hoso.net/2010/03/twittering-mode-url-jmp-bitly.html
