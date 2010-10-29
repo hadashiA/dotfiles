e3146f0e03132dfa90bcf07cbcecc44974826703
kubota <kubota@mario.local>
2010-10-29 10:41:38 +0900

    twitterign

--- emacs.d/conf/init-twitter.el
+++ emacs.d/conf/init-twitter.el
@@ -13,6 +13,18 @@
 (setq twittering-icon-mode nil)
 (setq twittering-scroll-mode nil)
 
+(add-hook 'twittering-mode-hook
+          (lambda ()
+            (mapc (lambda (pair)
+                    (let ((key (car pair))
+                          (func (cdr pair)))
+                      (define-key twittering-mode-map
+                        (read-kbd-macro key) func)))
+                  '(("F" . twittering-friends-timeline)
+                    ("R" . twittering-replies-timeline)
+                    ("U" . twittering-user-timeline)
+                    ("W" . twittering-update-status-interactive)))))
+
 ;; ;; いくつかのTLをまとめて名前をつけることができる
 ;; (setq twittering-timeline-spec-alias
 ;;       `(("related-to" .
