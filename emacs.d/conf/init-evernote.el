a14aaec6886924f3bb5a10a3fb38b2bd3a8774c9
kubota <kubota@mario.local>
2010-11-18 10:02:12 +0900

    init-evernote

--- emacs.d/conf/init-evernote.el
+++ emacs.d/conf/init-evernote.el
@@ -2,4 +2,5 @@
 (when (require 'evernote) nil t
       ;; (setq evernote-user "hadashikick"
       ;;       evernote-pass "hadashiex")
-      (global-set-key (kbd "C-c C-q") 'evernote-search))
+      (global-set-key (kbd "C-c C-q") 'evernote-search)
+      (global-set-key (kbd "C-c C-c C-c") 'evernote-quick-put))
