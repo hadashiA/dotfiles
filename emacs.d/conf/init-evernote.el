e0c038def3ae6abf2ce3a0e58b23d3d5102f338b
hadshiA <dev@hadashikick.jp>
2011-03-06 13:59:00 +0900

    emacs

--- emacs.d/conf/init-evernote.el
+++ emacs.d/conf/init-evernote.el
@@ -1,6 +1,4 @@
 (add-to-load-path "~/.emacs.d/elisp/evernote/")
 (when (require 'evernote) nil t
-      (setq evernote-user "hadashikick"
-            evernote-pass "hadashiex")
       (global-set-key (kbd "C-c C-q") 'evernote-search)
       (global-set-key (kbd "C-c C-c C-c") 'evernote-quick-put))
