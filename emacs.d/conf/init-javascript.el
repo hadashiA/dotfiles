c024e81b9f7dc30461f21676d1780fb4f15c6bb3
hadashi <12@hadashikick.jp>
2011-03-11 11:26:32 +0900

    javascript semicron and newline

--- emacs.d/conf/init-javascript.el
+++ emacs.d/conf/init-javascript.el
@@ -9,6 +9,14 @@
 
 (add-to-list 'auto-mode-alist '("\\.\\(js\\|as\\|json\\|jsn\\)\\'" . js2-mode))
 (require 'js2-mode)
+
+(defun insert-semicolon-and-new-line-and-indent ()
+  "insert semicolon and newline and indentation."
+  (interactive)
+  (insert ";")
+  (newline-and-indent)
+  )
+
 (add-hook 'js2-mode-hook
           '(lambda ()
              (setq js2-bounce-indent-flag nil
@@ -25,6 +33,7 @@
 
              (define-key js2-mode-map "\C-m" 'newline-and-indent)
              (define-key js2-mode-map "\C-i" 'indent-and-back-to-indentation)
+             (define-key js2-mode-map ";" 'insert-semicolon-and-new-line-and-indent)
              ))
 (defun indent-and-back-to-indentation ()
   (interactive)
