f6e068732bb6424660b5cda933b4f9a0ef8fa143
f.kubotar <f.kubotar@gmail.com>
2012-03-24 16:15:47 +0900

    add evernote subprof

--- emacs.d/conf/init-evernote.el
+++ emacs.d/conf/init-evernote.el
@@ -1,4 +1,17 @@
 (add-to-load-path "~/.emacs.d/elisp/evernote/")
-(when (require 'evernote) nil t
-      (global-set-key (kbd "C-c C-q") 'evernote-search)
-      (global-set-key (kbd "C-c C-c C-c") 'evernote-quick-put))
+;; (require 'evernote)
+;; (when (require 'evernote) nil t
+;;       (global-set-key (kbd "C-c C-q") 'evernote-search)
+;;       (global-set-key (kbd "C-c C-c C-c") 'evernote-quick-put))
+
+;; (add-to-load-path "~/.emacs.d/elisp/evernote-mode/")
+;; (require 'evernote-mode)
+;; (setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
+;; (global-set-key (kbd "C-c n c") 'evernote-create-note)
+;; (global-set-key (kbd "C-c n o") 'evernote-open-note)
+;; (global-set-key (kbd "C-c n s") 'evernote-search-notes)
+;; (global-set-key (kbd "C-c n S") 'evernote-do-saved-search)
+;; (global-set-key (kbd "C-c n w") 'evernote-write-note)
+;; (global-set-key (kbd "C-c n p") 'evernote-post-region)
+;; (global-set-key (kbd "C-c n b") 'evernote-browser)
+
