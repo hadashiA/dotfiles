3cd925d3aa790a7e85f51c3a33c0a976f3712cff
f.kubotar <f.kubotar@gmail.com>
2012-04-28 22:09:37 +0900

    fix typo

--- emacs.d/conf/init-markdown.el
+++ emacs.d/conf/init-markdown.el
@@ -1,8 +1,8 @@
 (autoload 'markdown-mode "markdown-mode.el"
    "Major mode for editing Markdown files" t)
 
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown--mode))
-(add-to-list 'auto-mode-alist '("\\.mkdn$" . markdown--mode))
-(add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown--mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown--mode))
-(add-to-list 'auto-mode-alist '("\\.text$" . markdown--mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mkdn$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
