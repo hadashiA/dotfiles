b377679a9433f21c4af4617e4fe4ec9e5aef7172
f.kubotar <f.kubotar@gmail.com>
2012-06-24 18:44:55 +0900

    copy.js customize liteal

--- vimperator/plugin/rcopy.js
+++ vimperator/plugin/rcopy.js
@@ -19,3 +19,31 @@
       }
     });
 })();
+
+
+(function () {
+  liberator.globalVariables.copy_templates = [
+    { label: 'titleAndURL',    value: '%TITLE%\n%URL%' },
+    { label: 'title',          value: '%TITLE%', map: ',y' },
+    { label: 'anchor',         value: '<a href="%URL%">%TITLE%</a>' },
+    { label: 'selanchor',      value: '<a href="%URL%" title="%TITLE%">%SEL%</a>' },
+    { label: 'htmlblockquote', value: '<blockquote cite="%URL%" title="%TITLE%">%HTMLSEL%</blockquote>' },
+    { label: 'ASIN',   value: 'copy ASIN code from Amazon',
+      custom: function(){
+        return content.document.getElementById('ASIN').value;}
+    },
+    { label: 'PA', value: 'copy account_id from CMSP',
+      custom: function() {
+        return (function (e, re) {
+          var m = e.textContent.match(re);
+          if (m) return m[0];
+          
+          for (var i = 0; i < e.children.length; i++) {
+            var result = arguments.callee(e.children[i], re);
+            if (result) return result;
+          }
+          return null;
+        })(content.document.body, /PA\d{8}/);
+      }}
+  ];
+})();
