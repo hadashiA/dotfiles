cd04f99960b2fe354027ad1075205e40c7fdfd28
久保田 竜自 <hadashi@mattaira.local>
2012-07-10 03:13:44 +0900

    auto-fu setting bugfix

--- zsh/common.zsh
+++ zsh/common.zsh
@@ -56,6 +56,55 @@ zstyle ':completion:*' completer _oldlist _complete _match _ignored \
 autoload -U compinit
 compinit
 
+# precompiled source
+function () { # precompile
+    local A
+    A=~/.zsh/modules/auto-fu/auto-fu.zsh
+    [[ -e "${A:r}.zwc" ]] && [[ "$A" -ot "${A:r}.zwc" ]] ||
+    zsh -c "source $A; auto-fu-zcompile $A ${A:h}" >/dev/null 2>&1
+}
+source ~/.zsh/modules/auto-fu/auto-fu; auto-fu-install
+
+# initialization and options
+function zle-line-init () { auto-fu-init }
+zle -N zle-line-init
+zstyle ':auto-fu:highlight' input bold
+zstyle ':auto-fu:highlight' completion fg=white
+zstyle ':auto-fu:var' postdisplay ''
+
+# afu+cancel
+function afu+cancel () {
+    afu-clearing-maybe
+    ((afu_in_p == 1)) && { afu_in_p=0; BUFFER="$buffer_cur"; }
+}
+function bindkey-advice-before () {
+    local key="$1"
+    local advice="$2"
+    local widget="$3"
+    [[ -z "$widget" ]] && {
+        local -a bind
+        bind=(`bindkey -M main "$key"`)
+        widget=$bind[2]
+    }
+    local fun="$advice"
+    if [[ "$widget" != "undefined-key" ]]; then
+        local code=${"$(<=(cat <<"EOT"
+            function $advice-$widget () {
+                zle $advice
+                zle $widget
+            }
+            fun="$advice-$widget"
+EOT
+        ))"}
+        eval "${${${code//\$widget/$widget}//\$key/$key}//\$advice/$advice}"
+    fi
+    zle -N "$fun"
+    bindkey -M afu "$key" "$fun"
+}
+bindkey-advice-before "^G" afu+cancel
+bindkey-advice-before "^[" afu+cancel
+bindkey-advice-before "^J" afu+cancel afu+accept-line
+
 # incremental completion
 if [[ $ZSH_VERSION == (<5->|4.<4->|4.3.<10->)* ]]; then
     function () { # precompile
