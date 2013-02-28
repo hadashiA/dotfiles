a66cf40a53a890df931860d85c692d954cd0a5d0
f-kubotar <dev@hadashikick.jp>
2013-02-28 23:40:15 +0900

    auto-save間隔のばす

--- emacs.d/conf/init-autosave-buffers.el
+++ emacs.d/conf/init-autosave-buffers.el
@@ -3,11 +3,11 @@
 ;; (when (require 'auto-save-buffers nil t)
 ;;   (run-with-idle-timer 0.5 t 'auto-save-buffers))
 
-(when (require 'auto-save-buffers-enhanced)
-  (auto-save-buffers-enhanced t)
-  (setq auto-save-buffers-enhanced-interval 2.0
-        auto-save-buffers-enhanced-include-regexps '(".+")
-        auto-save-buffers-enhanced-exclude-regexps '("^not-save-file" "\\.ignore$" ".*\\.howm$" "^save$" "\\.omm"))
-  (global-set-key "\C-xas" 'auto-save-buffers-enhanced-toggle-activity)
-  (run-with-idle-timer 2 t 'auto-save-buffers)
-)
+(setq auto-save-buffers-enhanced-interval 2.0
+      auto-save-buffers-enhanced-include-regexps '(".+")
+      auto-save-buffers-enhanced-exclude-regexps '("^not-save-file" "\\.ignore$" ".*\\.howm$" "^save$" "\\.omm"))
+
+(require 'auto-save-buffers-enhanced)
+(auto-save-buffers-enhanced t)
+(global-set-key "\C-xas" 'auto-save-buffers-enhanced-toggle-activity)
+
