65c562bb31a22bd0242d99e887873d8149955322
f-kubotar <f.kubotar@paperboy.co.jp>
2013-06-21 10:18:27 +0900

    update markdown-mode

--- emacs.d/conf/init-markdown.el
+++ emacs.d/conf/init-markdown.el
@@ -1,6 +1,3 @@
-(autoload 'markdown-mode "markdown-mode.el"
-   "Major mode for editing Markdown files" t)
-
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mkdn$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdwn$" . markdown-mode))
