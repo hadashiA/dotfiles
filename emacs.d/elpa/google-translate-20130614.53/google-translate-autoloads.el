1f5cacddb906bd8bfefa01d98a9a2fed3c492122
f-kubotar <f.kubotar@paperboy.co.jp>
2013-06-24 09:59:27 +0900

    revert translate

---
+++ emacs.d/elpa/google-translate-20130614.53/google-translate-autoloads.el
@@ -0,0 +1,18 @@
+;;; google-translate-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("google-translate-pkg.el" "google-translate.el")
+;;;;;;  (20927 63461 736414))
+
+;;;***
+
+(provide 'google-translate-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; google-translate-autoloads.el ends here
