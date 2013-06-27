75aa49188eb2d8f6f997bdd9df5eccd2945333eb
f-kubotar <f.kubotar@paperboy.co.jp>
2013-06-27 11:38:37 +0900

    expand-regionいれた

--- emacs.d/conf/init-region.el
+++ emacs.d/conf/init-region.el
@@ -1,1 +1,5 @@
+(global-set-key (kbd "C-@") 'er/expand-region)
+(global-set-key (kbd "C-`") 'er/contract-region) ;; リージョンを狭める
 
+;; transient-mark-modeが nilでは動作しませんので注意
+(transient-mark-mode t)
---
+++ emacs.d/elpa/expand-region-20130610.737/expand-region-pkg.el
@@ -0,0 +1,1 @@
+(define-package "expand-region" "20130610.737" "Increase selected region by semantic units. [github]" 'nil)
