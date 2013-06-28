fae901a4d5fc8cffb976234c91d50a1b339f6f9f
f-kubotar <dev@hadashikick.jp>
2013-06-28 23:25:33 +0900

    add multiple-cursors.el

--- emacs.d/conf/init-region.el
+++ emacs.d/conf/init-region.el
@@ -10,3 +10,8 @@
 ;; (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 ;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "M->") 'mc/mark-next-like-this)
+(global-set-key (kbd "M-<") 'mc/mark-previous-lines)
+(global-set-key (kbd "C-c M-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-@") 'set-rectangular-region-anchor)
