0dc68230ecf5c4df928b37e9b9493863e9eb5086
f-kubotar <dev@hadashikick.jp>
2013-06-29 15:04:58 +0900

    add web-mode

--- emacs.d/conf/init-html.el
+++ emacs.d/conf/init-html.el
@@ -13,10 +13,19 @@
 ;; ;; 勝手に雛形が作られてウザいのでnilにしておく
 ;; (setq html-helper-build-new-buffer nil)
 
-(add-hook 'html-mode-hook
-          (lambda ()
-            (define-key html-mode-map (kbd "M-n") 'sgml-skip-tag-forward)
-            (define-key html-mode-map "\M-p" 'sgml-skip-tag-backward)))
+;; (add-hook 'html-mode-hook
+;;           (lambda ()
+;;             (define-key html-mode-map (kbd "M-n") 'sgml-skip-tag-forward)
+;;             (define-key html-mode-map "\M-p" 'sgml-skip-tag-backward)))
 
 (require 'haml-mode)
 (require 'sass-mode)
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
---
+++ emacs.d/elpa/web-mode-20130627.2013/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20130627.2013" "major mode for editing html templates [github]" 'nil)
