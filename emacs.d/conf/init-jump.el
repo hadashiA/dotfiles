b0608bce22e6f560e058a00c87072fcc65e47697
f-kubotar <dev@hadashikick.jp>
2013-08-04 01:17:47 +0900

    ace-jump-mode keybind

--- emacs.d/conf/init-jump.el
+++ emacs.d/conf/init-jump.el
@@ -1,4 +1,4 @@
 (eval-after-load "ace-jump-mode"
   '(ace-jump-mode-enable-mark-sync))
 (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+(global-set-key (kbd "M-l") 'ace-jump-mode)
