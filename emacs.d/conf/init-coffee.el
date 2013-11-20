f56b677ef88cfb30e6199c5df4382adeb9487af8
f-kubotar <dev@hadashikick.jp>
2013-11-21 02:57:47 +0900

    coffee関連の設定追加

--- emacs.d/conf/init-coffee.el
+++ emacs.d/conf/init-coffee.el
@@ -15,7 +15,7 @@ Major mode for editing CoffeeScript.
               (setq coffee-tab-width 2)
               (setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
               (setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
-              (define-key coffee-mode-map (kbd "C-c o") #'ff-find-other-file)
+              (define-key coffee-mode-map (kbd "C-c o") #'coffee-compile-buffer)
 
               (define-key coffee-mode-map (kbd "C-c C-k") #'coffee-compile-file)
               (define-key coffee-mode-map (kbd "C-c C-l") #'coffee-build-for-grunt)
