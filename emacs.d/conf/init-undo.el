abbee0137c23c4c8dc9f6f3a1bfa54d6283f5d18
f-kubotar <dev@hadashikick.jp>
2014-02-24 00:16:45 +0900

    add undo-tree

--- emacs.d/conf/init-undo.el
+++ emacs.d/conf/init-undo.el
@@ -1,3 +1,6 @@
+(global-undo-tree-mode t)
+(global-set-key (kbd "M-/") 'undo-tree-redo)
+
 (when (require 'point-undo)
   (global-set-key (kbd "M-?") 'point-undo)
   (global-set-key (kbd "C-c M-?") 'point-redo))
