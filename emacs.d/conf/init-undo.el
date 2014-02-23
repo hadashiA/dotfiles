7c9a67f9822ee71ee7d2e81ed1b32be47b75e899
f-kubotar <dev@hadashikick.jp>
2014-02-24 00:18:19 +0900

    いくつかelpaにうつした

--- emacs.d/conf/init-undo.el
+++ emacs.d/conf/init-undo.el
@@ -1,10 +1,8 @@
 (global-undo-tree-mode t)
 (global-set-key (kbd "M-/") 'undo-tree-redo)
 
-(when (require 'point-undo)
-  (global-set-key (kbd "M-?") 'point-undo)
-  (global-set-key (kbd "C-c M-?") 'point-redo))
+(global-set-key (kbd "M-?") 'point-undo)
+(global-set-key (kbd "C-c M-?") 'point-redo)
 
-(when (require 'goto-last-change)
-  (global-set-key (kbd "C-?") 'goto-last-change))
+(global-set-key (kbd "C-?") 'goto-last-change)
 
