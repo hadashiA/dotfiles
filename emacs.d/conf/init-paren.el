890d1bf8ee82edaa0fbe345a9f620dd47fa61364
f-kubotar <dev@hadashikick.jp>
2014-03-01 15:18:35 +0900

    smartparensのキーバインド習性した

--- emacs.d/conf/init-paren.el
+++ emacs.d/conf/init-paren.el
@@ -1,6 +1,6 @@
 (require 'smartparens-config)
 (smartparens-global-mode t)
 
-(define-key sp-keymap (kbd "S-C-f") 'sp-forward-sexp)
-(define-key sp-keymap (kbd "S-C-b") 'sp-backward-sexp)
-(define-key sp-keymap (kbd "C-)") 'sp-unwrap-sexp)
+(define-key sp-keymap (kbd "S-C-f") 'sp-forward-symbol)
+(define-key sp-keymap (kbd "S-C-b") 'sp-backward-symbol)
+(define-key sp-keymap (kbd "C-9") 'sp-unwrap-sexp)
