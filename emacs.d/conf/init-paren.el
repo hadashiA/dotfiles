b9c4a74db2a18ef1621870d2b8202d878e3cc869
f-kubotar <dev@hadashikick.jp>
2014-03-02 23:16:38 +0900

    smartparensにクォートはずしとかあったのでキーバインド当てた

--- emacs.d/conf/init-paren.el
+++ emacs.d/conf/init-paren.el
@@ -1,8 +1,9 @@
 (require 'smartparens-config)
 (smartparens-global-mode t)
 
-(define-key sp-keymap (kbd "S-C-f") 'sp-forward-symbol)
-(define-key sp-keymap (kbd "S-C-b") 'sp-backward-symbol)
-(define-key sp-keymap (kbd "C-9") 'sp-unwrap-sexp)
+(define-key sp-keymap (kbd "S-C-f") 'sp-end-of-sexp)
+(define-key sp-keymap (kbd "S-C-b") 'sp-beginning-of-sexp)
+(define-key sp-keymap (kbd "C-9") 'sp-splice-sexp)
+(define-key sp-keymap (kbd "C-8") 'sp-backward-unwrap-sexp)
 
 
