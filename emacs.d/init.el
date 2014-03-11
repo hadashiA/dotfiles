1af751261e15bebd3aadb3ce8cb999d4580834d2
f-kubotar <dev@hadashikick.jp>
2014-03-11 23:14:36 +0900

    フォント設定追加した

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -203,3 +203,15 @@
  '(show-paren-match ((((class color) (background dark)) (:background "#3E3D32"))))
  '(variable-pitch ((t (:family "DejaVu Sans")))))
 (put 'narrow-to-region 'disabled nil)
+
+
+ 
+;; 日本語
+(set-fontset-font
+ nil 'japanese-jisx0208
+ ;; (font-spec :family "Hiragino Mincho Pro")) ;; font
+ (font-spec :family "Hiragino Kaku Gothic ProN")) ;; font
+ 
+;; 半角と全角の比を1:2に
+(setq face-font-rescale-alist
+      '((".*Hiragino_Mincho_pro.*" . 1.2)))
