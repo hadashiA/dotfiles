2c7938dc90b18d909f6ca2c5229da214ef9aa416
f-kubotar <dev@hadashikick.jp>
2014-08-10 13:07:37 +0900

    remove snippet

--- emacs.d/conf/init-paren.el
+++ emacs.d/conf/init-paren.el
@@ -3,6 +3,8 @@
 
 (define-key sp-keymap (kbd "S-C-f") 'sp-end-of-sexp)
 (define-key sp-keymap (kbd "S-C-b") 'sp-beginning-of-sexp)
+(define-key sp-keymap (kbd "S-C-d") 'sp-down-sexp)
+(define-key sp-keymap (kbd "S-C-u") 'sp-up-sexp)
 (define-key sp-keymap (kbd "C-9") 'sp-splice-sexp)
 (define-key sp-keymap (kbd "C-8") 'sp-backward-unwrap-sexp)
 
@@ -16,4 +18,3 @@
 (sp-local-pair 'c-mode "{" nil :post-handlers '((my/create-newline-and-enter-sexp "RET")))
 (sp-local-pair 'c++-mode "{" nil :post-handlers '((my/create-newline-and-enter-sexp "RET")))
 (sp-local-pair 'objc-mode "{" nil :post-handlers '((my/create-newline-and-enter-sexp "RET")))
-
