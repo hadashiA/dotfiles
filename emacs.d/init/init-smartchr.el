523c47781d4db01382ad436c1ae673e3205a83cd
f-kubotar <dev@hadashikick.jp>
2015-06-23 23:31:50 +0900

    es6 smartchr

--- emacs.d/init/init-smartchr.el
+++ emacs.d/init/init-smartchr.el
@@ -50,11 +50,12 @@
               (kbd "=") (smartchr '(" = " " == " " === " " != " " !== " "=")))
             (define-key js2-mode-map
               (kbd "+") (smartchr '("+" " + " "++" " += ")))
+            (define-key coffee-mode-map
+              (kbd "$") (smartchr '("$" "${`!!'}")))
+            (define-key coffee-mode-map
+              (kbd ".") (smartchr '("." " => " " (`!!') => ")))
             (define-key js2-mode-map
-              (kbd "{") (smartchr '("{ `!!' }" "{`!!'}")))
-            
-            (define-key js2-mode-map
-              (kbd "C-8") (smartchr '("(`!!');" "('`!!'');" "({ `!!' });" "(function(`!!') {  });")))
+              (kbd "C-8") (smartchr '("(`!!');\n" "('`!!'');\n" "({ `!!' });\n" "(function(`!!') {  });\n")))
             ))
 
 
