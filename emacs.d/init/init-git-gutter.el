c55f5e1df344622db9ad6c3fcfcc4d24c0345803
Syohei YOSHIDA <syohex@gmail.com>
2015-11-22 12:07:10 +0900

    Don't use deprecated API

--- emacs.d/init/init-git-gutter.el
+++ emacs.d/init/init-git-gutter.el
@@ -1,6 +1,6 @@
 (global-git-gutter-mode +1)
 
 (smartrep-define-key global-map  "C-x"
-  '(("p" . 'git-gutter:previous-diff)
-    ("n" . 'git-gutter:next-diff)))
+  '(("p" . 'git-gutter:previous-hunk)
+    ("n" . 'git-gutter:next-hunk)))
 
