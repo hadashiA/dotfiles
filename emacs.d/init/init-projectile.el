256ebfe3270708c7809a67a1b44aaece03203bdb
hadashiA <dev@hadashikick.jp>
2016-10-09 18:02:44 +0900

    projectile globally ignore files..

--- emacs.d/init/init-projectile.el
+++ emacs.d/init/init-projectile.el
@@ -1,10 +1,12 @@
 (setq projectile-completion-system 'helm
       projectile-enable-caching t
       projectile-use-native-indexing t
-      projectile-indexing-method 'native)
+      projectile-indexing-method 'alien
+      projectile-globally-ignored-file-suffixes '(".meta" ".dll" ".pbd" ".csproj" ".fbx" ".FBX")
+      )
 
 (projectile-global-mode)
 
 (define-key projectile-mode-map (kbd "C-c o") 'projectile-toggle-between-implementation-and-test)
 
-(setq projectile-indexing-method 'alien)
+
